ce07142b46bb60d3119b7c56a3c37adf5e5191f6
Justin McCarthy <mccar085@cougars.csusm.edu>
2019-11-05 18:14:58 -0800

    JSON response with delay

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+//const { Kanjiapi } = require('kanjiapi-wrapper');
+const { grade1, grade2, grade3, grade4, grade5, grade6, grade7 } = require('./kanji.json');
 const{ prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
@@ -8,10 +10,50 @@ client.once('ready', () => {
 
 var schedule = require('node-schedule');
 
-var j = schedule.scheduleJob('/0 * * * * *', function(){
-    printDate();
+var http = require('http');
+
+let data = '';
+
+var j = schedule.scheduleJob('/0 * * * * *', () =>{
+    //printDate();
     console.log('date printed');
+    var options = {
+        host: 'kanjiapi.dev',
+        port: '80',
+        path: encodeURI('/v1/kanji/貯'),
+        method: 'GET'
+      };
+
+    var req = http.request(options, function(res) {
+        //res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          //console.log('BODY: ' + chunk);
+          jResponse = JSON.parse(chunk);
+        });
+      }).end();
+
+      setTimeout(func1, 3000);
+      console.log(jResponse);
 });
+
+function func1(){
+    console.log(jResponse);
+}
+
+
+
+/*
+const listener = () => {
+    // Ask kanjiapi instance for data again and trigger UI redraws
+}
+
+const kanjiapi = Kanjiapi.build()
+kanjiapi.addListener('listener_name', listener)
+
+const result = kanjiapi.getKanji('字')
+
+console.log(result);
+*/
 
 function printDate() {
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
